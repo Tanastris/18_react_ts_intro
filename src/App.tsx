740aba8456ae93d58293b87fa3e569eaa2e213bf
RokasV <vezikauskas.r@gmail.com>
2024-02-20 08:40:26 +0200

    simple props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div className="container">
-      <h1 className="display-2">React dev app</h1>
-    </div>
-  )
+type HeadingProps = {
+  title: string;
+};
+
+function Heading(props: HeadingProps) {
+  console.log('props ===', props);
+  return <h3>{props.title}</h3>;
 }
 
-export default App
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='display-2'>React dev app</h1>
+      <Heading title='Sveiki' />
+    </div>
+  );
+}
+
+export default App;
